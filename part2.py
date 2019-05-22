5df13453c5a0d48b64a92827cb9e4637f5a74baf
Uğurcan Arıkan <ugurcanarikan95@gmail.com>
2019-05-23 01:55:31 +0300

    major changes on method structure and tf-idf score computing

--- part2.py
+++ part2.py
@@ -1,45 +1,28 @@
 import os, json, math
+import numpy as np
 
 CORPUS_PATH = "corpus/derlem.txt"
+WEIGHTS_PATH = 'weights/'
+GLOVE_PATH = 'glove/'
 QA_PATH = "corpus/soru_gruplari.txt"
 punctuations = "\"!^%<+~*;:(?&}]|,')-#`@/$_{.>[\="
-DOCUMENT_NUMBER = 0
 
-df = {}
-tf = {}
-tf_idf = {}
-
-def CalculateTfIdf(n):
-    for paragraph_id in tf.keys():
-        w = {} # score dictionary
-        length = 0.0 # length of the tf.idf vector
-        for word in df.keys():
-            tf_ = 0
-            idf = math.log10(n/df[word])
-            
-            if word in tf[paragraph_id]:
-                tf_ = 1 + math.log10(tf[paragraph_id][word])
-            
-            score = tf_ * idf
-            
-            if score > 0.1: # threshold
-                w[word] = score
-                length += w[word]**2
-        
-        length = math.sqrt(length)
-        
-        for word in w.keys(): # normalization
-            w[word] = w[word]/length 
-        
-        tf_idf[paragraph_id] = w # add it to the tf_idf
-
-def CreateFiles():
+def createFiles(tf, df, tf_idf):
     for f, fname in zip([tf,df,tf_idf],["tf.json", "df.json", "tf_idf.json"]):
-        o = open(os.path.join("Output", fname), "w", encoding="utf-16")
+        o = open(os.path.join("weights", fname), "w", encoding="utf-16")
         o.write(json.dumps(f, indent=4, sort_keys=True))
         o.close()
 
-def Freq(paragraph):
+def loadFiles(weights_path):
+    with open(weights_path + 'tf.json') as f1:  
+        tf = json.load(f1)
+    with open(weights_path + 'df.json') as f2:  
+        df = json.load(f2)
+    with open(weights_path + 'tf_idf.json') as f3:  
+        tf_idf = json.load(f3)
+    return tf_idf, tf, df
+
+def freq(df, paragraph):
     term_freq = {}
     for word in paragraph.split():
         if word in term_freq:
@@ -50,10 +33,41 @@ def Freq(paragraph):
             else:
                 df[word] = 1
             term_freq[word] = 1
-    return term_freq
+    return term_freq, df
 
-def ReadCorpus(path):
-    global DOCUMENT_NUMBER
+def calculateTfIdf(tf, df, doc_num):
+    tf_idf = {}
+    for paragraph_id in tf.keys():
+        w = {} # score dictionary
+        length = 0.0 # length of the tf.idf vector
+        for word in df.keys():
+            tf_ = 0
+            idf = math.log10(doc_num/df[word])
+            
+            if word in tf[paragraph_id]:
+                tf_ = 1 + math.log10(tf[paragraph_id][word])
+            
+            score = tf_ * idf
+            w[word] = score
+            '''
+            length += w[word]**2
+            
+            if score > 0.1: # threshold
+                w[word] = score
+                
+        
+        length = math.sqrt(length)
+        
+        for word in w.keys(): # normalization
+            w[word] = w[word]/length 
+        '''
+        tf_idf[paragraph_id] = w # add it to the tf_idf
+    return tf_idf, tf, df, doc_num
+
+def readCorpus(path):
+    tf = {}
+    df = {}
+    doc_num = 0
     corpus = open(path, "r", encoding="utf-16")
     for line in corpus:
         if line == "\n":
@@ -62,36 +76,9 @@ def ReadCorpus(path):
             line = line.replace(c, " ")
         line = line.lower()
         paragraph_id, paragraph = line.split(maxsplit=1)
-        tf[paragraph_id] = Freq(paragraph)
-        DOCUMENT_NUMBER += 1
-    CalculateTfIdf(DOCUMENT_NUMBER)
-
-def Sentence2TfIdf(s):
-    tfidf_s = {}
-    tf = {}
-    idf = {}
-    for c in punctuations:
-        s = s.replace(c, " ")
-    s = s.lower()
-    word_count = len(s.split())
-    
-    for word in s.split():
-        tf.setdefault(word, 0)
-        tf[word] = tf[word] + 1
-        if word in df.keys():
-            idf[word] = math.log(DOCUMENT_NUMBER / df[word])
-        else:
-            idf[word] = math.log(DOCUMENT_NUMBER / 1)
-    for word in tf.keys():
-        tfidf_s[word] = tf[word] * idf[word]
-    return tfidf_s
-    
-def cosine_prox(dict1, dict2): # inner product of two dictionaries
-    sum = 0.0 
-    for word in dict1.keys():
-        if word in dict2:
-            sum += dict1[word] * dict2[word]
-    return sum
+        tf[paragraph_id], df = freq(df, paragraph)
+        doc_num += 1
+    return calculateTfIdf(tf, df, doc_num)
 
 def read_qa(path):
     qa = {}
@@ -106,53 +93,89 @@ def read_qa(path):
             qa[line[:line.find(":")]] = [line[line.find(":") + 2:], answer[answer.find(":") + 2:], paragraph[paragraph.find(":") + 2:]]
     return qa
 
-    """
-    for line in corpus.split("\n\n"):
-        if line[0] == "S":
-            qa[line[:line.find(":")]] =   line[line.find(":") + 2: line.find("\n")]      
-    print(qa)
-    """
+def sentence2TfIdf(df, doc_num, s):
+    tfidf_s = {}
+    tf = {}
+    idf = {}
+    for c in punctuations:
+        s = s.replace(c, " ")
+    s = s.lower()
+    word_count = len(s.split())
+    #length = 0.0
+    for word in s.split():
+        tf.setdefault(word, 0)
+        tf[word] = tf[word] + 1
+        if word in df.keys():
+            idf[word] = math.log10(doc_num / df[word])
+        
+        else:
+            idf[word] = math.log10(doc_num / 1)
+        
+    for word in idf.keys():
+        #tfidf_s[word] = (1 + math.log(tf[word])) * idf[word]
+        tfidf_s[word] = tf[word] * idf[word]
+        #length += tfidf_s[word]**2
+    
+    #length = math.sqrt(length)
 
-ReadCorpus(CORPUS_PATH)
+    for word in tfidf_s.keys(): # normalization
+        tfidf_s[word] = tfidf_s[word]#/length 
+    return tfidf_s
+    
+def cosine_similarity(dict1, dict2): # inner product of two dictionaries
+    result = 0.0 
+    '''
+    lenght1 = np.linalg.norm(list(dict1.values()))
+    lenght2 = np.linalg.norm(list(dict2.values()))
+    '''
+    for word in dict1.keys():
+        if word in dict2:
+            result += dict1[word] * dict2[word]
+    return result #/ (lenght1 * lenght2)
+
+def loadGloveModel(gloveFile):
+    print("Loading Glove Model")
+    f = open(gloveFile,'r')
+    model = {}
+    for line in f:
+        splitLine = line.split()
+        word = splitLine[0]
+        embedding = np.array([float(val) for val in splitLine[1:]])
+        model[word] = embedding
+    print("Done.",len(model)," words loaded!")
+    return model
+
+def find_paragraph(df, tf_idf, doc_num, sentence):
+    scores = {}
+    for key in tf_idf.keys(): 
+        scores[key] = cosine_similarity(sentence2TfIdf(df, doc_num, sentence), tf_idf[key])
+    
+    sorted_x = sorted(scores.items(), key=lambda kv: kv[1])
+    sorted_x.reverse()
+    return sorted_x[0][0]
+#vector = loadGloveModel(GLOVE_PATH + 'vectors.txt')
+#print(vector['ve'])
+
+tf_idf, tf, df, doc_num = readCorpus(CORPUS_PATH)
+#tf_idf, tf, df = loadFiles(WEIGHTS_PATH)
+
 qa = read_qa(QA_PATH)
-print(qa["S426"])
 
 t = 0
 f = 0
 print("qa time")
 
-for key in qa.keys():
+for key1 in qa.keys():
     scores = {}
-    for id in tf_idf.keys(): 
-        scores[id] = cosine_prox(Sentence2TfIdf(qa[key][0]), tf_idf[id])
-    
+    for key2 in tf_idf.keys(): 
+        scores[key2] = cosine_similarity(sentence2TfIdf(df, doc_num, qa[key1][0]), tf_idf[key2])
+    print(key1)
     sorted_x = sorted(scores.items(), key=lambda kv: kv[1])
     sorted_x.reverse()
-    print(key)
-    print(qa[key][2])
-    print(sorted_x[0][0])
-    if sorted_x[0][0] == qa[key][2]:
+    if sorted_x[0][0] == qa[key1][2]:
         t = t + 1
     else:
         f = f + 1
-print(t)
-print(f)
-# CreateFiles()
 
-
-
-"""
-Q = input("Type your question (To stop type END)")
-
-while Q != "END":
-    scores = {} # scores of all movies
-    
-    for id in tf_idf.keys(): 
-        scores[id] = cosine_prox(Sentence2TfIdf(Q), tf_idf[id])
-    
-    sorted_x = sorted(scores.items(), key=lambda kv: kv[1])
-    sorted_x.reverse()
-
-    print([x[0] for x in sorted_x[:10]])
-    Q = input("Type your question (To stop type END)")
-"""
+    print(t)
+    print(f)
